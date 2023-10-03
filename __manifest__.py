84329684be0c686e7ba19334eb59c0d032e111cd
Rastafara <sergey.rastafara@gmail.com>
2023-10-03 13:11:14 +0300

    price

--- __manifest__.py
+++ __manifest__.py
@@ -18,6 +18,8 @@
     'version': '16.0.0.37',
 	'license': 'LGPL-3',
 	'images': ['images/thumb.png'],
+	'price': 10.00,
+	'currency': 'EUR',
     # any module necessary for this one to work correctly
     'depends': ['base',
                 #'account',
