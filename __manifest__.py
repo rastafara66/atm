6ce02ca7820d2f6515a126be0466c0afd617cbc3
Rastafara <sergey.rastafara@gmail.com>
2023-10-03 13:13:49 +0300

    depends accounts

--- __manifest__.py
+++ __manifest__.py
@@ -22,7 +22,7 @@
 	'currency': 'EUR',
     # any module necessary for this one to work correctly
     'depends': ['base',
-                #'account',
+                'account',
                 'sale',
 	],
 
