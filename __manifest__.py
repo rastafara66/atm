1ad6aaff24e3381ab33180f4b815e6f8ab9e3fb5
Rastafara <sergey.rastafara@gmail.com>
2023-10-04 15:20:37 +0300

    upd

--- __manifest__.py
+++ __manifest__.py
@@ -30,10 +30,10 @@
     'data': [
         'views/views.xml',
         'views/templates.xml',
-		'views/atm_settings.xml',
         'data/cron.xml',
         #'data/sale_demo.xml',
         'security/ir.model.access.csv',
+		'views/atm_settings.xml',
     ],
     # only loaded in demonstration mode
     'demo': [
