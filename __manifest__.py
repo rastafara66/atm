efcc6992a67e04cb775342803f98af112b8bd52c
Rastafara <sergey.rastafara@gmail.com>
2023-10-05 11:05:31 +0300

    upd from settings

--- __manifest__.py
+++ __manifest__.py
@@ -32,9 +32,7 @@
         'views/views.xml',
         'views/templates.xml',
 		'views/res_config_settings_views.xml',
-        'data/cron.xml',
-        #'data/sale_demo.xml',
-        'security/ir.model.access.csv',		
+        'data/cron.xml',	
     ],
     # only loaded in demonstration mode
     'demo': [
