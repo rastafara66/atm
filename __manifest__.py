8adbae2a48cd94f5f9c0d19f1103f7295de9afa0
Rastafara <sergey.rastafara@gmail.com>
2023-10-26 11:51:17 +0300

    final version 16.0.0.100

--- __manifest__.py
+++ __manifest__.py
@@ -6,7 +6,7 @@
     'author': "chukhin",
     'website': "https://github.com/rastafara66",
     'category': 'Tools, Specific Industry Applications',
-    'version': '16.0.0.103',
+    'version': '16.0.0.100',
 	'license': 'LGPL-3',
 	'images': [
 		'images/thumb.png',
