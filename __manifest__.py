98ba7c9589c7d65c4191b68311489f30cc11ba3d
Rastafara <sergey.rastafara@gmail.com>
2023-12-01 12:52:00 +0200

    upd

--- __manifest__.py
+++ __manifest__.py
@@ -6,7 +6,7 @@
     'author': "chukhin",
     'website': "https://github.com/rastafara66",
     'category': 'Tools',
-    'version': '16.0.1.23.11.62',
+    'version': '16.0.1.23.11.82',
 	'license': 'LGPL-3',
 	'images': [
 		'images/thumb.png',
